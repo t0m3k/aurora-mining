1a0515a189e98ec84fd1472dc0bf0f3f6ec57aee
Tomasz Tracz <t@tracz.me>
2017-12-15 11:31:49 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import PoolList from './PoolList';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <PoolList />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/PoolList.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class PoolList extends Component {
+    render(){
+        return (
+            <h1>Pool List!</h1>
+        )
+    }
+}
+
+export default PoolList;
