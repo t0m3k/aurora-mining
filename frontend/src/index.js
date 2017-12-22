f5ef0b71b2a4b99ecd0f73ac306cb419544350a5
Tomasz Tracz <t@tracz.me>
2017-12-22 13:24:17 +0000

    Remove style from index file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import reducer from './reducers';
