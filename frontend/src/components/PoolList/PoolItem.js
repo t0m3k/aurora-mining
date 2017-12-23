53c07f405e0d1a5a432f917a49fb3438c75e3271
Tomasz Tracz <t@tracz.me>
2017-12-23 16:32:18 +0000

    Display currency in earnings segment

--- frontend/src/components/PoolList/PoolItem.js
+++ frontend/src/components/PoolList/PoolItem.js
@@ -34,7 +34,7 @@ const PoolItem = ({
             ]
         },
         {
-            header: "Earnings " + currency,
+            header: "Earnings - " + currency,
             values: [
                 {
                     value: (usdPerMin * 60 * 24 * rate).toFixed(2),
@@ -51,7 +51,7 @@ const PoolItem = ({
             ]
         },
         {
-            header: "Earnings " + coin,
+            header: "Earnings - " + coin,
             values: [
                 {
                     value: (coinsPerMin * 60 * 24).toFixed(4),
@@ -99,6 +99,7 @@ const PoolItem = ({
     return  <div className="item eight wide computer sixteen wide tablet column">
                 <div className="content">
                     <div className="ui segments">
+
                         <div className="ui segment">
                             
                             <h2 className="ui medium header">{ name }</h2>
@@ -120,10 +121,13 @@ const Segment = ({header, values, address}) => {
     const stats = values.map((value) => (<Stat key={value.label + value.value + address} {...value}/>))
     return (
         <div className="ui segment">
+        
             <p className="ui header">{header}</p>
+
             <div className="ui grid centered mini statistics">
                 {stats}
             </div>
+            
         </div>
     )
 }
