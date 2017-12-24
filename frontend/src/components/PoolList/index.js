20ce6c1d6c54dfb1b2474f895f251072213ed296
Tomasz Tracz <t@tracz.me>
2017-12-24 11:43:06 +0000

    Add redux

--- frontend/src/components/PoolList/index.js
+++ frontend/src/components/PoolList/index.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
 import PoolItem from './PoolItem';
 import pools from '../../controllers/pools';
-import {timeCounter} from '../../controllers/helper';
+import { connect } from 'react-redux'; 
+import { timeCounter } from '../../controllers/helper';
+import * as user from '../../actions/user'
 import './index.css';
-
 
 class PoolList extends Component {
     constructor(props){
@@ -58,20 +59,21 @@ class PoolList extends Component {
     }
 
     componentDidMount(){
-        this.getCurrency();
-        this.showPoolsFrames();
-        this.loadPools();
+        this.props.dispatch(user.fetchUser.bind(this));
+        // this.getCurrency();
+        // this.showPoolsFrames();
+        // this.loadPools();
     }
 
     // will load pool info from user and display frames for them
     showPoolsFrames(){
-        return this.setState((prevState) => ({...prevState, pools:[...prevState.user.pools]}));
+        this.setState((prevState) => ({...prevState, pools:[...prevState.user.pools]}));
     }
     
     // will load pools data from cache
     loadPools(){
-        this.state.user.pools.map(pool => {
-            return pools.get[pool._id.pool](pool._id.address)
+        this.state.user.pools.forEach(pool => {
+            pools.get[pool._id.pool](pool._id.address)
             .then(loaded => this.addPool({...loaded, name: pool.name, index: pool.index}));
         })
     }
@@ -81,13 +83,13 @@ class PoolList extends Component {
         .then(resp => {
             if(!resp.ok) {
                 if(resp.status >=400 && resp.status < 500) {
-                return resp.json().then(data => {
-                    let err = {errorMessage: data.message};
-                    throw err;
-                })
+                    resp.json().then(data => {
+                        let err = {errorMessage: data.message};
+                        throw err;
+                    })
                 } else {
-                let err = {errorMessage: 'Please try again later, server is not responding'};
-                throw err;
+                    let err = {errorMessage: 'Please try again later, server is not responding'};
+                    throw err;
                 }
             }
             resp.json()
@@ -112,9 +114,6 @@ class PoolList extends Component {
     refreshPool(pool){
         return pools.fresh[pool._id.pool](pool._id.address)
             .then(loaded => {
-
-                console.log("Refreshed pool: " + pool.name)
-
                 return this.addPool({...loaded, name: pool.name, index: pool.index}, false)
             });
     }
@@ -122,16 +121,11 @@ class PoolList extends Component {
     // r - if true check if pool needs to be refreshed and refresh 
     addPool(pool, r = true){
 
-        console.log("Adding pool: " + pool.name, pool.index)
-
-        var pools = this.state.pools.filter(item => {
+        let pools = this.state.pools.filter(item => {
             return !((item._id.address === pool._id.address) && (item._id.pool === pool._id.pool))
         });
 
-        if(r && pool.updTime && timeCounter(pool.updTime, 10)){
-
-            console.log("Need to refresh pool: " + pool.name)
-
+        if((r && !pool.updTime) || (r && timeCounter(pool.updTime, 2))){
             this.refreshPool(pool);
         }
 
@@ -139,6 +133,7 @@ class PoolList extends Component {
     }
 
     render(){
+        console.log(this.props)
         const pools = this.state.pools.map((pool) => (
           <PoolItem
             key = {pool._id.address}
@@ -147,9 +142,10 @@ class PoolList extends Component {
             rate = {this.state.currency.rate}
           />
           ));
-          pools.sort((a, b) => a.props.index - b.props.index)
-          console.log(pools);
-        return ( 
+
+          pools.sort((a, b) => a.props.index - b.props.index);
+
+        return (
         <div className="ui container">
             <div className="ui grid stackable">
                     {pools}
@@ -159,4 +155,10 @@ class PoolList extends Component {
     }
 }
 
-export default PoolList;
+const mapStateToProps = state => {
+    return {
+        user: state.user
+    }
+}
+
+export default connect(mapStateToProps)(PoolList);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
-import reducer from './reducers';
+import {createStore, applyMiddleware} from 'redux';
+import {createLogger as logger} from 'redux-logger';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
 
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(reducer);
+const middleware = applyMiddleware(thunk, logger());
+
+const store = createStore(reducers, middleware);
 
 ReactDOM.render(
 <Provider store={store}>
